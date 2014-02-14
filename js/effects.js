606989300e844355cd867442a9e333ef61c08ff8
Rigas Papas <rigaspapas@gmail.com>
2014-02-14 21:34:23 +0200

    Changed the design once again

--- js/effects.js
+++ js/effects.js
@@ -1,78 +1,99 @@
-/* Function that makes the correct element to fade in and the others to fade out */
-function navigate ( goal , easing ) {
-	var menuItem = "#nav-" + goal;
-	$("#navigator li").removeClass("selected");
-	$(menuItem).addClass("selected");
-		
-	var elementToFocus = '#' + goal + '-element';
-	$(".column").removeClass("selected");
-	$(elementToFocus).addClass("selected");
-	
-    var movePosition = 0-$(elementToFocus).position().left;
-	$("#content").stop().animate({'left':movePosition} , easing )
-	
-    if (goal == 'work')
-        $('.headerGradient').removeClass('green').addClass('blue');
+var dirtyScrolling = false;
+
+function detect_mobile() { 
+    var user_agent = navigator.userAgent.toString();
     
-    else if (goal == 'contact')
-        $('.headerGradient').removeClass('blue').addClass('green');
-        
+    if(user_agent.match(/Android/i)
+    || user_agent.match(/webOS/i)
+    || user_agent.match(/iPhone/i)
+    || user_agent.match(/iPad/i)
+    || user_agent.match(/iPod/i)
+    || user_agent.match(/BlackBerry/i)
+    || user_agent.match(/Windows Phone/i)
+    )
+        return true;
     else
-        $('.headerGradient').removeClass('blue green');
-    
-    return;
+        return false;
 }
 
-
-$(document).ready( function () {
-	$("#work-element h3").click( function () {
-		$(this).toggleClass("opened").next("div.work-cont").slideToggle( 400 );
-	});
+/* Function that makes the correct element to fade in and the others to fade out */
+function navigate ( goal , easing ) {
 	
-	$("#nav-bio , #bio-element").click( function () { navigate( "bio" , 1000) });
-	$("#nav-work , #work-element").click( function () { navigate( "work" , 1000) });
-	$("#nav-contact , #contact-element").click( function () { navigate( "contact" , 1000) });
+    var menuItem = '#nav-' + goal,
+        elementToFocus = '#' + goal + '-article';
+	
+    $( '#navigator li, .article' ).removeClass( 'selected' );
+	$( menuItem + ',' + elementToFocus ).addClass( 'selected' );
+    
+    if ( easing === false )
+        return false;
+    
+    dirtyScrolling = true;
+    
+    $( 'html, body' ).stop().animate( {
+            scrollTop: $( elementToFocus ).offset().top - $( '#header' ).height() + 'px'
+        },
+        easing,
+        function () {
+            dirtyScrolling = false;
+        }
+    );
+    
+    return true;
+}
+
+$( document ).ready( function () {
+    
+    if ( detect_mobile() )
+        $( 'body' ).attr( 'id', 'mobile' );
+
+	$( '#nav-bio , #bio-article' ).click( function () { navigate( 'bio' , 1000 ) });
+	$( '#nav-work , #work-article' ).click( function () { navigate( 'work' , 1000 ) });
+	$( '#nav-contact , #contact-article' ).click( function () { navigate( 'contact' , 1000 ) });
 	
 	/* Hide the elements we don't want to be shown */
 	var page = window.location.hash.substring(1);
-	if ((page != "work") && (page != "contact")) page = "bio";
-	navigate( page , 300);
+	if ( ( page != 'work' ) && ( page != 'contact' ) ) page = "bio";
 	
-	handleOverflow();
-	
+    navigate( page , 300 );
+		
 	var hash = location.hash;
 	
     /* We check periodically if the hash has the changed in order to navigate dynamically */
 	setInterval( function() {
-		if (location.hash != hash) {
+		if ( location.hash != hash ) {
 			hash = location.hash;
-			$('#navigator li').each(function(){
-				var dest = $(this).attr("id").substring(4);
-				if ( "#"+dest == hash )
+			
+            $( '#navigator li' ).each( function(){
+				var dest = $( this ).attr( 'id' ).substring( 4 );
+				
+                if ( '#' + dest == hash )
 					navigate( dest , 500 );
-			});
+			} );
 		}
-	}, 100);
+	}, 100 );
 } );
 
-function handleOverflow() {
-	if ( $(window).width() > 600 )
-		$("body").css({'overflow-x':'hidden'});
-	else
-		$("body").css({'overflow-x':'auto'});
-        
-    var w = $( window ).width();
-    
-    if ( w > 550 )
-        $( '#contentWrapper' ).css({
-            'width' : w + 'px',
-            'overflow' : 'hidden'
-        });
-}
+$( window ).on( 'resize scroll', function () {
 
-$( window ).resize( function () {
-	handleOverflow();
-    /* Recover the focused element and re-focus on it */
-	var focused = $('#navigator li[class="selected"]').attr("id");
-	navigate( focused.substring(4) , 500 );
-});
+    if ( dirtyScrolling )
+        return;
+    
+    var min = Math.abs( $( '#bio-article' ).offset().top - 180 - $( window ).scrollTop() );
+    var focused = 'bio-article';
+    
+    $( '.article' ).each( function () {
+    
+        var dist = Math.abs( $( this ).offset().top - 180 - $( window ).scrollTop() );
+    
+        if ( dist < min ) {
+            min = dist;
+            focused = $( this ).attr( 'id' );
+        }
+    } );
+    
+    console.log( focused.substring( 0, focused.length - 8 ) );
+    
+    navigate( focused.substring( 0, focused.length - 8 ), false );
+
+} );
