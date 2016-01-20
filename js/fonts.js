bb794ba48a74e5f73e5f2a907af9ec752de1f411
rigaspapas <rigaspapas@gmail.com>
2016-01-20 14:40:55 +0200

    Remove any jQuery dependencies from JS files

--- js/fonts.js
+++ js/fonts.js
@@ -1,7 +1,7 @@
-$(document).ready( function () {
+window.onload = function(){
 	var fontello = document.createElement('link');
 	fontello.setAttribute('rel', 'stylesheet');
 	fontello.setAttribute('type', 'text/css');
 	fontello.setAttribute('href', 'css/fontello.css');
 	document.getElementsByTagName('head')[0].appendChild( fontello );
-});
+}
