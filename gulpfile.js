af38fbc26689419e16d08484405feb34e86bc519
Rigas Papas <rigaspapas@gmail.com>
2017-08-31 17:47:09 +0300

    Change built process and website structure
    Redesign the front page.
    Remove unused CSS stuff.
    Use CSS autoprefixer.
    Ignore built files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,60 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 
+const SRC = 'src/';
+const DEST = 'dist/';
+
 gulp.task('pug2html', () => {
-  return gulp.src('pug/*.pug')
+  return gulp.src(SRC + 'pug/*.pug')
     .pipe(pug({
       filename: '*.html',
       pretty: false,
     }))
-    .pipe(gulp.dest('..'));
+    .pipe(gulp.dest(DEST));
 });
 
 // Compile LESS into minified CSS
 gulp.task('less2css', () => {
-  return gulp.src('less/style.less')
+  return gulp.src(SRC + 'less/style.less')
     .pipe(less({
       compress: true,
+    }))
+    .pipe(autoprefixer({
+      browsers: ['Last 5 versions',
+                 '> 1%'],
     }))
     .pipe(rename({
       suffix: '.min',
     }))
-    .pipe(gulp.dest('../css/'));
+    .pipe(gulp.dest(DEST + 'css/'));
 });
 
 // Optimize images
 gulp.task('optimize-images', () => {
-  return gulp.src('images/**/*')
+  return gulp.src(SRC + 'images/**/*')
     .pipe(imagemin([
       imagemin.jpegtran({progressive: true}),
       imagemin.optipng({optimizationLevel: 5}),
     ], {
       verbose: true
     }))
-    .pipe(gulp.dest('../images/'));
+    .pipe(gulp.dest(DEST + 'images/'));
 });
 
+// Copy fonts
+gulp.task('copy-fonts', () => {
+  return gulp.src(SRC + 'fonts/**/*')
+    .pipe(gulp.dest(DEST + 'fonts'));
+});
 
 gulp.task('default', () => {
     gulp.start('build');
-    gulp.watch('pug/*.pug', ['pug2html']);
-    gulp.watch('less/*.less', ['less2css']);
+    gulp.watch(SRC + 'pug/*.pug', ['pug2html']);
+    gulp.watch(SRC + 'less/*.less', ['less2css']);
 });
 
-gulp.task('build', ['pug2html', 'less2css', 'optimize-images']);
+gulp.task('build', ['pug2html', 'less2css', 'optimize-images', 'copy-fonts']);
